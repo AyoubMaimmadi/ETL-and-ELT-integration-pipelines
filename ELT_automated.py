51ac08c179843f62291a967f1a3db37314804f60
elamraniadnane1 <elamraniadnane1@gmail.com>
2024-02-17 00:42:28 +0100

    VF

--- ELT_automated.py
+++ ELT_automated.py
@@ -1,24 +1,36 @@
 import subprocess
+import os
 
 def run_hadoop_command(command):
     try:
         subprocess.run(command, check=True, shell=True)
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e}")
-    except FileNotFoundError as e:
-        print(f"Error: {e}. Make sure Hadoop commands are installed and configured properly.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
 
-# Step 1: Load data into HDFS (example command)
-load_command = "hadoop fs -copyFromLocal C:\\Users\\LENOVO\\ETL-and-ELT-integration-pipelines\\sales_csv /sales_data"
-run_hadoop_command(load_command)
+def step_1():
+    hdfs_path = '/sales_data'
+    local_path = "C:\\Users\\LENOVO\\ETL-and-ELT-integration-pipelines\\sales_csv"
+    # Check if directory exists in HDFS
+    if subprocess.run(f"hadoop fs -test -d {hdfs_path}", shell=True).returncode != 0:
+        load_command = f"hadoop fs -copyFromLocal {local_path} {hdfs_path}"
+        run_hadoop_command(load_command)
+    else:
+        print(f"{hdfs_path} already exists in HDFS. Skipping copy.")
 
-# Step 2: Copying Mapper.py into HDFS
-load_mapper = "hadoop fs -put C:\\Users\\LENOVO\\ETL-and-ELT-integration-pipelines\\mapper.py /python"
-run_hadoop_command(load_mapper)
+def step_2():
+    hdfs_path = '/python'
+    local_file = "C:\\Users\\LENOVO\\ETL-and-ELT-integration-pipelines\\mapper.py"
+    # Check if file exists in HDFS
+    if subprocess.run(f"hadoop fs -test -e {hdfs_path}", shell=True).returncode != 0:
+        load_mapper = f"hadoop fs -put {local_file} {hdfs_path}"
+        run_hadoop_command(load_mapper)
+    else:
+        print(f"{hdfs_path} already exists in HDFS. Skipping copy.")
 
-# Step 3: Run Hadoop Streaming job for transformation
-streaming_command = "hadoop jar C:\\Users\\LENOVO\\hadoop-3.3.0\\hadoop-3.3.0\\share\\hadoop\\tools\\lib\\hadoop-streaming-3.3.0.jar -files file:/C:/Users/LENOVO/ETL-and-ELT-integration-pipelines/mapper.py -mapper \"python mapper.py\" -input /sales_data/* -output /output-sales-data"
-run_hadoop_command(streaming_command)
+def step_3():
+    streaming_command = 'hadoop jar C:\\Users\\LENOVO\\hadoop-3.3.0\\hadoop-3.3.0\\share\\hadoop\\tools\\lib\\hadoop-streaming-3.3.0.jar -files file:/C:/Users/LENOVO/ETL-and-ELT-integration-pipelines/mapper.py -mapper "python mapper.py" -input /sales_data/* -output /output-sales-data'
+    run_hadoop_command(streaming_command)
 
+step_1()
+step_2()
+step_3()
